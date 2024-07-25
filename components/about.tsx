db5a1670013f4b754a44f9cd9d97033f3b92e7ac
logan3124 <ltweekes1@gmail.com>
2024-07-25 16:22:48 -0400

    first

---
+++ components/about.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        I am currently studying{" "}
+        <span className="font-medium">Computer Science</span> at Georiga Tech with focuses in
+        {" "}
+        <span className="font-medium">Artificial Intelligence</span> and{" "}
+        <span className="font-medium">Media Applications</span> in addition to a minor in Spanish.
+        Currently I am focusing on backend-engineering and recently completed my certification as an
+        {" "}<span className="font-medium">AWS Certified Developer</span> at the Associate level.
+      </p>
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy
+        distance running, hiking, and cooking. I also enjoy{" "}
+        <span className="font-medium">reading</span> a mix of fiction and non-fiction books. 
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,109 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import AWS from "./AWS.png";
+import { LuGraduationCap } from "react-icons/lu";
+import punchshot from '@/public/punchshot.png';
+import croplocal from '@/public/croplocal.png';
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import realappeal from "@/public/realappeal.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "GT Web Development Club - Project Manager",
+    location: "Atlanta, GA",
+    description:
+      "Active club member for over two years. Co-led a team of around 8 students in a three semester long client project called Punchshot Pickleball.",
+    icon: React.createElement(LuGraduationCap),
+    date: "January 2023 - May 2024",
+  },
+  {
+    title: "AWS Certified Developer Associate",
+    location: "N/A",
+    description:
+      "Completed Amazon's certification with a passing score of 832/1000.",
+    icon: React.createElement(CgWorkAlt),
+    date: "December 2023 - May 2024",
+  },
+  {
+    title: "RVO (Red Venture Optum) Health - Software Engineering Intern",
+    location: "Charlotte, NC",
+    description:
+      "Completed a 10-Week internship program working as a Full-Stack developer for the Real Appeal application. Worked on post-migration of the site over to Next.js while boosting our web-accessibility scores to over 95% according to WCAG 2.0",
+    icon: React.createElement(FaReact),
+    date: "June 2024 - August 2024 ",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Punchshot Pickleball",
+    description:
+      "Client project aimed to make Pickleball leagues more accessible in the Atlanta area. Features: league registration, automated match scheduling, stats tracking ...",
+    tags: ["React", "Typescript", "MaterialUI", "AWS S3", "MongoDB"],
+    imageUrl: punchshot,
+  },
+  {
+    title: "Crop Local",
+    description:
+      "Aimed to connect consumers with their favorite farmers market vendors. Features: Buy/Sells Goods, Filter stands by location, reserve items for pickup at local markets.",
+    tags: ["React", "Javascript", "SASS", "MongoDB", "GoogleMaps API"],
+    imageUrl: croplocal,
+  },
+  {
+    title: "Real Appeal Developer",
+    description:
+      "Worked on Migration of flagship application onto newer tech stack. Tasks: Migrated Splunk alerts over to Datadog monitors and further configured user event tracking. ",
+    tags: ["Next.js", "Tailwind", "Typescript", "Amplitude", "Datadog", "Splunk"],
+    imageUrl: realappeal,
+  },
+] as const;
+
+export const skillsData = [
+  "AWS Databases and S3",
+  "Encryption and IAM",
+  "CloudWatch/CloudTrail",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Datadog Logging",
+  "Amplitude",
+  "Github Actions",
+  "Tailwind",
+  "MongoDB",
+  "Express",
+  "Python",
+] as const;
